e028c21c2cdedd0410002d82c6dd847040f189d4
Sarthak Agarwal <sarthak.agarwal97@gmail.com>
2019-09-19 01:27:02 +0530

    modulazrize express

--- server/index.js
+++ server/index.js
@@ -1,1 +1,24 @@
-console.log("hello");
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+const http = require('http');
+const port = 4000;
+const host = 'localhost';
+
+app.use(bodyParser.json());
+app.all('/dishes',(req,res, next) =>{
+     console.log(req.headers);
+     res.statusCode = 200;
+     res.setHeader('Content-Type','text/html');
+     next();
+});
+app.get('/dishes',(req, res,next) =>{
+ res.end("you are getting the right dish");
+});
+
+
+const server = http.createServer(app);
+server.listen(port , host , () =>{
+    console.log(`server is listen to port http://${host}:${3000}`);
+})
